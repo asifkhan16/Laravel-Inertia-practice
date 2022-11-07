9c2fd234f6db49e554a2fa2346a4e1a943933c06
asif khan <asiff080@gmail.com>
2022-11-07 10:52:01 +0500

    changes in app.js and vite.config to run the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,14 @@
+import('./bootstrap');
+
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
+  resolve: name => import(`./Pages/${name}.vue`),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
 })
+
